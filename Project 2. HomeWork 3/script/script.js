b3359f29d2ea1f21809b1649cfafee1958ebcd05
tcybulnyk2201 <113143973+tcybulnyk2201@users.noreply.github.com>
2022-10-10 13:43:43 +0300

    Add files via upload

--- Project 2. HomeWork 3/script/script.js
+++ Project 2. HomeWork 3/script/script.js
@@ -1,9 +1,14 @@
+$('.space_items > div').on('click', function(){
+    $(this).next('.space_description').slideToggle(300);
+})
+
+
+
 $('.about-items > div').on('click', function(){
-    $(this).children('.about-icon').toggleClass('about-rotate-icon');
     $(this).parent('.about-items').toggleClass('change-color');
+    $(this).children('.about-icon').toggleClass('about-rotate-icon');
     $(this).children('.about-items_title').toggleClass('change-color-text');
-    $(this).children('.about-items_text').toggleClass('change-color-text');
-    $(this).children('.about-items_description').toggleClass('change-color-text');
-    $(this).children('.about-icon').toggleClass('change-color-text');
     $(this).next('.about-items_text').slideToggle(300);
 })
+
+
